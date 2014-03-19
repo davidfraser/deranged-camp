e99d9e37d2525df091819c155a4beda4d784f729
David Fraser <davidf@sjsoft.com>
2014-03-19 14:19:54 +0200

    Add license header (anticipating matt's commits)

--- student_derangement.py
+++ student_derangement.py
@@ -2,6 +2,20 @@
 
 """Take N students and split them up into G groups, A times (for different activities), so that no two students are in the same group more than once"""
 
+# Copyright 2014 David Fraser <https://github.com/davidfraser/> and Matthew Hampton <https://github.com/matthewhampton/>
+# 
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not 
+# use this file except in compliance with the License. You may obtain a copy 
+# of the License at 
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software 
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+# License for the specific language governing permissions and limitations 
+# under the License. 
+
 import math
 import pprint
 import random
